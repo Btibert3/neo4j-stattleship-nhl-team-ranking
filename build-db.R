af6c88407b57f6207def27b3220f472123b11641
Btibert3 <btibert3@gmail.com>
2016-01-14 19:10:39 -0500

    update for blog md

--- build-db.R
+++ build-db.R
@@ -7,12 +7,12 @@
 options(stringsAsFactors = FALSE)
 
 # un-comment below to install the packages if necessary
-install.packages("devtools")
-devtools::install_github("stattleship/stattleship-r")
-install.packages("dplyr")
-install.packages("lubridate")
-install.packages("stringr")
-devtools::install_github("nicolewhite/RNeo4j")
+# install.packages("devtools")
+# devtools::install_github("stattleship/stattleship-r")
+# install.packages("dplyr")
+# install.packages("lubridate")
+# install.packages("stringr")
+# devtools::install_github("nicolewhite/RNeo4j")
 
 ## packages -- if errors, see above
 library(stattleshipR)
@@ -23,7 +23,7 @@ library(RNeo4j)
 
 ## set the token from an environment variable
 set_token(Sys.getenv("STATTLE_TOKEN"))
-# set_token("yourtokenhere")
+
 
 ###############################################################################
 ## A few helper functions 
@@ -38,11 +38,11 @@ parse_stattle <- function(stattle_list, entry) {
 
 ## helper function to import cql files against neo4j import shell tool
 ## set NEO_SHELL to your locally running neo4j server shell
-# build_import <- function(NEO_SHELL="~/neo4j-community-2.3.1/bin/neo4j-shell", 
-#                          cypher_file) {
-#   cmd = sprintf("%s -file %s", NEO_SHELL, cypher_file)
-#   system(cmd)
-# }
+build_import <- function(NEO_SHELL="~/neo4j-community-2.3.1/bin/neo4j-shell", 
+                         cypher_file) {
+  cmd = sprintf("%s -file %s", NEO_SHELL, cypher_file)
+  system(cmd)
+}
 
 ###############################################################################
 ## A few calls against the Stattleship API gives us everything we need
@@ -52,7 +52,6 @@ parse_stattle <- function(stattle_list, entry) {
 games_ss <- ss_get_result(ep="games", 
                           query=list(status="ended"), 
                           walk = TRUE)
-
 
 ## parse out the games and keep the columns we want (regular season only)
 games <- parse_stattle(games_ss, "games") %>% 
@@ -69,7 +68,17 @@ games <- parse_stattle(games_ss, "games") %>%
          away_team_score,
          score_differential,
          home_team_outcome,
-         away_team_outcome)
+         away_team_outcome) 
+
+## extract dateparts from started date
+games <- transform(games, 
+                   start_date = strptime(started_at, "%Y-%m-%dT%H:%M:%S"))
+games <- transform(games,
+                   year = year(start_date),
+                   month = month(start_date),
+                   day = day(start_date))
+games$start_date <- NULL
+games$started_at <- NULL
 
 ## parse out the teams from the games API -- need to do that just once
 teams <- parse_stattle(games_ss, "home_teams") %>% 
@@ -79,18 +88,6 @@ teams <- parse_stattle(games_ss, "home_teams") %>%
          name, 
          nickname, 
          slug)  
-
-## extract dateparts from started date
-games <- transform(games, 
-                   start_date = strptime(started_at, "%Y-%m-%dT%H:%M:%S"))
-games <- transform(games,
-                   year = year(start_date),
-                   month = month(start_date),
-                   day = day(start_date))
-
-## make the games dataset
-games$started_at <- NULL
-games$start_date <- NULL
 
 
 ## write the datafiles as csvs for neo import and future use
